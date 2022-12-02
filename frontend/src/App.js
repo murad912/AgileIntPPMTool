6e876ada4908193cf9ef57e19f2b2988e596b0ad
16418 <muradabdulmalik@gmail.com>
2022-12-02 16:57:55 -0600

    react code change

---
+++ frontend/src/App.js
@@ -0,0 +1,8 @@
+import "./App.css";
+import Dashboard from "./components/Dashboard";
+import "bootstrap/dist/css/bootstrap.min.css";
+function App() {
+  return <Dashboard />;
+}
+
+export default App;
